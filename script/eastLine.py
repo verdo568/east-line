669010e7b812d66a8488db6567ccfa28eaaded22
Sam <75374659+Sam123www@users.noreply.github.com>
2025-04-01 10:55:48 +0800

    123

--- script/eastLine.py
+++ script/eastLine.py
@@ -1,14 +1,18 @@
 import pygame
 import random
+import os
 import time
 
-const write = 1
-const turnHead = 2
-const minTime = 5
-const maxTime = 15
-static = write
+pygame.init()
+screen = pygame.display.set_mode((1600, 900))
 
-def EastLineStatic:
+write = 1
+turnHead = 2
+minTime = 5
+maxTime = 15
+status = write
+
+def EastLineStatus():
     if(static == write){
         time.sleep(random.randint(minTime, maxTime))
         static = turnHead
