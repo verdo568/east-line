351a010d1e663d4ea1ec8f50922762f453d4e69c
Sam <75374659+Sam123www@users.noreply.github.com>
2025-04-08 22:22:26 +0800

    123

--- script/eastLine.py
+++ script/eastLine.py
@@ -1,15 +1,25 @@
 import pygame
 import random
-import os
 import time
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 900))
+screen.fill((255, 255, 255))
+screen_rect = screen.get_rect()
 
+bg = pygame.Surface(screen.get_size())
+bg = bg.convert()
+bg.fill((255, 255, 255))
+font = pygame.font.Font(('font\JasonHandwriting1.ttf'), 50)
 
 class Player:
     def __init__(self):
         pass
+    def update(self):
+        userInput = pygame.key.get_pressed()
+        if userInput[pygame.K_SPACE]:
+            ProgressBar.lookUp = True
+            
 
 class EastLine:
     def __init__(self):
@@ -28,16 +38,23 @@ class EastLine:
 
 class ProgressBar:
     def __init__(self):
-        global step, delayTime, timer
-        self.timer = pygame.time.get_ticks()
-        self.delayTime = 500
+        global step, delayTime, timer, lookUp
         self.step = 0
+        self.timer = pygame.time.get_ticks()
+        self.delayTime = 200
+        self.lookUp = False
     def progressBar(self):
-        if(pygame.time.get_ticks() > self.timer):
+        if pygame.time.get_ticks() > self.timer and self.lookUp == False:
             self.step += 1
             self.timer = pygame.time.get_ticks() + self.delayTime
-        pygame.draw.rect(screen, (192, 192, 192), (5, 100, 490, 20))
-        pygame.draw.rect(screen, (0, 0, 255), (5, 100, min(5+self.step, 490), 20))
+            text = font.render(str(int(self.step/8)), True, '#000000', '#ffffff')
+            screen.blit(text, (0, 0))
+        left = screen_rect.centerx - 400
+        width = 800
+        height = 20
+        buttom = screen_rect.bottom
+        pygame.draw.rect(screen, (192, 192, 192), (left, buttom - 50, width, height))
+        pygame.draw.rect(screen, (0, 0, 255), (left, buttom - 50, min(self.step, width), height))
     def update(self):
         ProgressBar.progressBar(self)
 
@@ -48,12 +65,11 @@ def main():
     run = True
     while run:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False;
-        screen.fill((255, 255, 255))
-        userInput = pygame.key.get_pressed()
+              if event.type == pygame.QUIT:
+                 run = False;
+        player.update()
         progressBar.update()
         pygame.display.update()
+
+pygame.display.update()
 main()
-screen.fill((255, 255, 255))
-#test
