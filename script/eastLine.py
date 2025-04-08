deadcf503875b60d93a1b44f87762db7d8a95d3f
Sam <75374659+Sam123www@users.noreply.github.com>
2025-04-08 11:35:16 +0800

    123

--- script/eastLine.py
+++ script/eastLine.py
@@ -6,41 +6,53 @@ import time
 pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 
+
 class Player:
     def __init__(self):
         pass
-    
 
 class EastLine:
     def __init__(self):
         global write, turnHead, minTime, maxTime, status
-        write = 1
-        turnHead = 2
-        minTime = 5
-        maxTime = 15
-        status = write
-
-    def eastLineStatus():
-        if(static == write):
+        self.write = 1
+        self.turnHead = 2
+        self.minTime = 5
+        self.maxTime = 15
+        self.status = self.write
+    def eastLineStatus(self):
+        if(self.status == write):
             time.sleep(random.randint(minTime, maxTime))
-            static = turnHead
-        elif(static == turnHead):
+            self.status = turnHead
+        elif(self.status == turnHead):
             pass
-    
-    def update(self, userInput):
-        if userInput[pygame.K_SPACE]:
-            pass
+
+class ProgressBar:
+    def __init__(self):
+        global step, delayTime, timer
+        self.timer = pygame.time.get_ticks()
+        self.delayTime = 500
+        self.step = 0
+    def progressBar(self):
+        if(pygame.time.get_ticks() > self.timer):
+            self.step += 1
+            self.timer = pygame.time.get_ticks() + self.delayTime
+        pygame.draw.rect(screen, (192, 192, 192), (5, 100, 490, 20))
+        pygame.draw.rect(screen, (0, 0, 255), (5, 100, min(5+self.step, 490), 20))
+    def update(self):
+        ProgressBar.progressBar(self)
 
 def main():
     player = Player()
-    run = True;
+    eastline = EastLine()
+    progressBar = ProgressBar()
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False;
         screen.fill((255, 255, 255))
         userInput = pygame.key.get_pressed()
-        player.updata(userInput)
+        progressBar.update()
         pygame.display.update()
 main()
 screen.fill((255, 255, 255))
