c9e195c31fae10b654a37e4e7cb5d4692ed33c49
Sam <75374659+Sam123www@users.noreply.github.com>
2025-04-10 01:43:10 +0800

    123

--- script/eastLine.py
+++ script/eastLine.py
@@ -6,39 +6,35 @@ pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 screen.fill((255, 255, 255))
 screen_rect = screen.get_rect()
-
-bg = pygame.Surface(screen.get_size())
-bg = bg.convert()
-bg.fill((255, 255, 255))
-font = pygame.font.Font(('font\JasonHandwriting1.ttf'), 50)
+font = pygame.font.Font(('font\JasonHandwriting1.ttf'), 24)
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, pb):
+        self.pb = pb
     def update(self):
         userInput = pygame.key.get_pressed()
         if userInput[pygame.K_SPACE]:
-            ProgressBar.lookUp = True
+            self.pb.lookUp = True
+        else:
+            self.pb.lookUp = False
             
 
 class EastLine:
     def __init__(self):
-        global write, turnHead, minTime, maxTime, status
         self.write = 1
         self.turnHead = 2
         self.minTime = 5
         self.maxTime = 15
         self.status = self.write
     def eastLineStatus(self):
-        if(self.status == write):
-            time.sleep(random.randint(minTime, maxTime))
-            self.status = turnHead
-        elif(self.status == turnHead):
+        if(self.status == self.write):
+            time.sleep(random.randint(self.minTime, self.maxTime))
+            self.status = self.turnHead
+        elif(self.status == self.turnHead):
             pass
 
 class ProgressBar:
     def __init__(self):
-        global step, delayTime, timer, lookUp
         self.step = 0
         self.timer = pygame.time.get_ticks()
         self.delayTime = 200
@@ -47,26 +43,30 @@ class ProgressBar:
         if pygame.time.get_ticks() > self.timer and self.lookUp == False:
             self.step += 1
             self.timer = pygame.time.get_ticks() + self.delayTime
-            text = font.render(str(int(self.step/8)), True, '#000000', '#ffffff')
-            screen.blit(text, (0, 0))
         left = screen_rect.centerx - 400
         width = 800
         height = 20
         buttom = screen_rect.bottom
         pygame.draw.rect(screen, (192, 192, 192), (left, buttom - 50, width, height))
-        pygame.draw.rect(screen, (0, 0, 255), (left, buttom - 50, min(self.step, width), height))
+        if self.lookUp:
+            pygame.draw.rect(screen, (255, 0, 0), (left, buttom - 50, min(self.step, width), height))
+        else:
+            pygame.draw.rect(screen, (0, 0, 255), (left, buttom - 50, min(self.step, width), height))
+        text = font.render(f'進度: {float(self.step/8):.1f}%', True, '#000000')
+        screen.blit(text, (left + width + 5, buttom - 55))
     def update(self):
         ProgressBar.progressBar(self)
 
 def main():
-    player = Player()
     eastline = EastLine()
     progressBar = ProgressBar()
+    player = Player(progressBar)
     run = True
     while run:
         for event in pygame.event.get():
               if event.type == pygame.QUIT:
                  run = False;
+        screen.fill((255, 255, 255))
         player.update()
         progressBar.update()
         pygame.display.update()
