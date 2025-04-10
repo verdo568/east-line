bebbf41a3580ca662ce54ba67c321317ca9f02e6
Sam <75374659+Sam123www@users.noreply.github.com>
2025-04-11 01:35:41 +0800

    今天進度飛快

--- script/eastLine.py
+++ script/eastLine.py
@@ -2,44 +2,103 @@ import pygame
 import random
 import time
 
+#初始化
 pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 screen.fill((255, 255, 255))
 screen_rect = screen.get_rect()
+#載入字體
 font = pygame.font.Font(('font\JasonHandwriting1.ttf'), 24)
+#載入動畫
+EastLine_writting = [pygame.image.load("Image\writting.png"), pygame.image.load("Image\writting2.png")]
+EastLine_look = pygame.image.load("Image\look.png")
+EastLine_find = pygame.image.load("Image\\find.png")
+EastLine_down = pygame.image.load("Image\down.png")
+#場景管理
 
-class Player:
-    def __init__(self, pb):
-        self.pb = pb
-    def update(self):
-        userInput = pygame.key.get_pressed()
-        if userInput[pygame.K_SPACE]:
-            self.pb.lookUp = True
-        else:
-            self.pb.lookUp = False
-            
 
+#東線
 class EastLine:
     def __init__(self):
+        self.image = EastLine_writting[0]
         self.write = 1
-        self.turnHead = 2
+        self.down = 2
+        self.look = 3
+        self.find = 4
         self.minTime = 5
         self.maxTime = 15
         self.status = self.write
-    def eastLineStatus(self):
-        if(self.status == self.write):
-            time.sleep(random.randint(self.minTime, self.maxTime))
-            self.status = self.turnHead
-        elif(self.status == self.turnHead):
-            pass
+        self.WriteId = 0
+        self.animationTimer = pygame.time.get_ticks()
+        self.statusTimer = pygame.time.get_ticks()
+        self.animationDelayTime = 200
+        self.firstSwitch = True
+    #切換狀態
+    def update(self):
+        match self.status:
+            #寫
+            case self.write:
+                if pygame.time.get_ticks() > self.animationTimer:
+                    self.animationTimer = pygame.time.get_ticks() + self.animationDelayTime
+                    self.WriteId = (self.WriteId + 1) % 2
+                    self.image = EastLine_writting[self.WriteId]
+                if self.firstSwitch:
+                    self.firstSwitch = False
+                    self.statusTimer = pygame.time.get_ticks() + random.randint(3000, 6000)
+                #切換
+                if pygame.time.get_ticks() > self.statusTimer:
+                    self.status = self.down
+                    self.firstSwitch = True
+            #放下粉筆
+            case self.down:
+                self.image = EastLine_down
+                if self.firstSwitch:
+                    self.firstSwitch = False
+                    self.statusTimer = pygame.time.get_ticks() + random.randint(1000, 2000)
+                #切換
+                if pygame.time.get_ticks() > self.statusTimer:
+                    #1/3的機率回頭
+                    nextStatus = random.randint(1, 3)
+                    if nextStatus == 1:
+                        self.status = self.look
+                    else:
+                        self.status = self.write
+                    self.firstSwitch = True
+            #觀察
+            case self.look:
+                self.image = EastLine_look
+                if self.firstSwitch:
+                    self.firstSwitch = False
+                    self.statusTimer = pygame.time.get_ticks() + random.randint(1000, 3000)
+                #切換
+                if pygame.key.get_pressed()[pygame.K_SPACE] == False:
+                    self.status = self.find
+                    self.firstSwitch = True
+                if pygame.time.get_ticks() > self.statusTimer:
+                    self.status = self.write
+                    self.firstSwitch = True
+                #找到
+            case self.find:
+                self.image = EastLine_find
+                pygame.time.wait(2000)
+                
+        screen.blit(self.image, screen_rect.center)
+                
 
+
+#進度條
 class ProgressBar:
     def __init__(self):
         self.step = 0
         self.timer = pygame.time.get_ticks()
         self.delayTime = 200
         self.lookUp = False
-    def progressBar(self):
+    def update(self):
+        userInput = pygame.key.get_pressed()
+        if userInput[pygame.K_SPACE] == True:
+            self.lookUp = True
+        elif userInput[pygame.K_SPACE] == False:
+            self.lookUp = False
         if pygame.time.get_ticks() > self.timer and self.lookUp == False:
             self.step += 1
             self.timer = pygame.time.get_ticks() + self.delayTime
@@ -54,20 +113,17 @@ class ProgressBar:
             pygame.draw.rect(screen, (0, 0, 255), (left, buttom - 50, min(self.step, width), height))
         text = font.render(f'進度: {float(self.step/8):.1f}%', True, '#000000')
         screen.blit(text, (left + width + 5, buttom - 55))
-    def update(self):
-        ProgressBar.progressBar(self)
 
 def main():
     eastline = EastLine()
     progressBar = ProgressBar()
-    player = Player(progressBar)
     run = True
     while run:
         for event in pygame.event.get():
               if event.type == pygame.QUIT:
                  run = False;
         screen.fill((255, 255, 255))
-        player.update()
+        eastline.update()
         progressBar.update()
         pygame.display.update()
 
